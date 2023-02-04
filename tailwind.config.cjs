dada13369b47b36064b14b2bb5a3cb3d206d171f
calz18 <40797702+calz18@users.noreply.github.com>
2023-02-04 13:02:31 +0530

    config updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,25 @@
 module.exports = {
     content: ['./src/routes/**/*.{svelte,js,ts}'],
-    plugins: [require('daisyui')],
+    plugins: [require("@tailwindcss/typography"), require('daisyui')],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+
+                    "primary": "#00923c",
+
+                    "secondary": "#D926AA",
+
+                    "accent": "#1FB2A5",
+
+                    "neutral": "#191D24",
+
+                    "base-100": "#2A303C",
+
+
+                },
+            },
+        ],
+    },
+
 };
